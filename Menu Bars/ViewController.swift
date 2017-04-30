b3ea39957ae5fc30984abc5c5ccfd06ac0d4f660
Noah Schutte <noahschutte@gmail.com>
2017-04-30 12:28:05 -0600

    initial commit

--- Menu Bars/ViewController.swift
+++ Menu Bars/ViewController.swift
@@ -9,6 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    var seconds = 210
+    
+    
+    @IBAction func leftNavPressed(_ sender: Any) {
+        
+        timer.invalidate()
+        
+    }
+    
+    @IBAction func rightNavPressed(_ sender: Any) {
+    
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    func processTimer() {
+        
+        if seconds > 0 {
+        
+            seconds -= 1
+        
+            secondsDisplay.text = String(seconds)
+            
+        } else {
+            
+            timer.invalidate()
+            
+        }
+    }
+    
+    @IBOutlet var secondsDisplay: UILabel!
+    
+    @IBAction func decrement10Seconds(_ sender: Any) {
+        
+        if seconds > 10 {
+         
+            seconds -= 10
+            
+            secondsDisplay.text = String(seconds)
+            
+        }
+        
+    }
+
+    @IBAction func resetTimer(_ sender: Any) {
+        
+        seconds = 210
+        
+        secondsDisplay.text = String(seconds)
+        
+    }
+    
+    @IBAction func increment10Seconds(_ sender: Any) {
+        
+        seconds += 10
+        
+        secondsDisplay.text = String(seconds)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
